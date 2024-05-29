06ec1dfff92759c9d76811205f4eca480393789b
Wiktor <darkvikos@gmail.com>
2024-05-29 13:11:43 +0200

    func: propper ground looping; somehwat functional intro screen

--- src/main/kotlin/Game/Main.kt
+++ src/main/kotlin/Game/Main.kt
@@ -3,18 +3,19 @@ package Game
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Alignment
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.*
-import androidx.compose.ui.input.key.key
-import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.key.KeyEventType
-import androidx.compose.ui.input.key.type
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import kotlinx.coroutines.flow.Flow
@@ -22,23 +23,119 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.*
 import androidx.compose.ui.graphics.drawscope.*
+import androidx.compose.ui.input.key.*
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.flow.first
 import kotlin.math.max
 import kotlin.math.pow
 
+enum class ViewState {
+    INTRO,
+    GAME
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-@Preview
-fun App(game: Game) {
-    Surface() {
-        KtLander(game)
+fun App() {
+    var viewState by remember { mutableStateOf(ViewState.INTRO) }
+
+    when (viewState) {
+        ViewState.INTRO -> IntroView(onStartGame = { viewState = ViewState.GAME })
+        ViewState.GAME -> GameView(onBackToIntro = { viewState = ViewState.INTRO })
     }
 }
 
-class Game {
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun IntroView(onStartGame: () -> Unit) {
+    val focusRequester = remember { FocusRequester() }
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(
+            text = "Press SPACE to Start",
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxSize().focusRequester(focusRequester).focusable().onKeyEvent {
+                if (it.type == KeyEventType.KeyDown && it.key == Key.Spacebar) {
+                    onStartGame()
+                    true
+                } else {
+                    false
+                }
+            }
+        )
+        LaunchedEffect(Unit) {
+            focusRequester.requestFocus()
+        }
+    }
+}
+
+@Composable
+fun endText(text: String) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(
+            text = "Safely Landed!",
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
+
+@Composable
+fun GameView(onBackToIntro: () -> Unit) {
+    val focusRequester = remember { FocusRequester() }
+    val game = Game(onBackToIntro)
+    Surface(modifier = Modifier.focusRequester(focusRequester).focusable().onKeyEvent {
+        when (it.key) {
+            Key.A -> {
+                when (it.type) {
+                    KeyEventType.KeyDown -> {
+                        game.rotationToApply = -35.0
+                    }
+                    KeyEventType.KeyUp -> {
+                        game.rotationToApply = 0.0
+                    }
+                }
+                true
+            }
+            Key.D -> {
+                when (it.type) {
+                    KeyEventType.KeyDown -> {
+                        game.rotationToApply = 35.0
+                    }
+                    KeyEventType.KeyUp -> {
+                        game.rotationToApply = 0.0
+                    }
+                }
+                true
+            }
+            Key.W -> {
+                when (it.type) {
+                    KeyEventType.KeyDown -> {
+                        game.engineTime = 1.0
+                    }
+                    KeyEventType.KeyUp -> {
+                        game.engineTime = 0.0
+                    }
+                }
+                true
+            }
+            else -> {
+                false
+            }
+        }
+    }) {
+        KtLander(game)
+        LaunchedEffect(Unit) {
+            focusRequester.requestFocus()
+        }
+    }
+}
+
+class Game(onBackToIntro: () -> Unit) {
     private val coroutineScope = CoroutineScope(Dispatchers.Default)
 
     private val lunar = Lunar(generateSurface(), 1737000.0, 7.34767309 * 10.0.pow(22))
@@ -58,17 +155,15 @@ class Game {
     var fps = MutableStateFlow(30)
     var lastTime = MutableStateFlow(System.currentTimeMillis())
 
-
     init {
         coroutineScope.launch {
 
             while (true) {
                 val currentTime = System.currentTimeMillis()
                 val elapsedTime = currentTime - lastTime.value
-                fps.value = frameCount.value * 1000 / max(elapsedTime.toInt(), 1)
-//                fps.value = 0
 
                 if (elapsedTime >= 1000) {
+                    fps.value = frameCount.value * 1000 / max(elapsedTime.toInt(), 1)
                     frameCount.value = 0
                     lastTime.value = currentTime
                 }
@@ -89,9 +184,7 @@ class Game {
 
 //                gravitationalAcceleration = gravitationalAcceleration(lunar.mass, lunar.radius, mutableState.value.first.position.second)
                 gravitationalAcceleration = -0.00167
-//                println(gravitationalAcceleration)
-                var lFps = max(fps.value, 1)
-                val newVelocity = calcNewVelocity(mutableState.value.first, (engineTime / lFps), gravitationalAcceleration)
+                val newVelocity = calcNewVelocity(mutableState.value.first, (engineTime / fps.value), gravitationalAcceleration)
                 var newXPosition = (mutableState.value.first.position.first + (newVelocity.first * time)) % maxX
                 if (newXPosition < 0) {
                     newXPosition += maxX
@@ -103,16 +196,16 @@ class Game {
                                 newXPosition,
                                 it.first.position.second + (newVelocity.second * time)
                             ),
-                            rotation = (it.first.rotation + (rotationToApply / lFps)) % 360.0,
+                            rotation = (it.first.rotation + (rotationToApply / fps.value)) % 360.0,
                             velocity = newVelocity),
                         Pair(
                             it.second.first,
                             engineTime
                         ))
                 }
-                println("X: ${mutableState.first().first.position.first}")
-                println("Y: ${mutableState.first().first.position.second}")
             }
+            delay(2000)
+            onBackToIntro()
         }
     }
 }
@@ -134,7 +227,6 @@ fun Board(state: Pair<Lander, Pair<List<Pair<Double, Double>>, Double>>, onFrame
     val textMeasurer = rememberTextMeasurer()
     val textVelocityX = "vx: %.2f".format(state.first.velocity.first)
     val textVelocityY = "vx: %.2f".format(state.first.velocity.second)
-//    val textFps = "FPS: $fps"
 
     val style = TextStyle(
         fontSize = 16.sp,
@@ -149,9 +241,6 @@ fun Board(state: Pair<Lander, Pair<List<Pair<Double, Double>>, Double>>, onFrame
         textMeasurer.measure(textVelocityY, style)
     }
 
-//    val textLayoutFps = remember(textFps, style) {
-//        textMeasurer.measure(textFps, style)
-//    }
 
     Canvas(Modifier.fillMaxSize().background(Color.hsl(237F, 1F, 0.08F))) {
         onFrame()
@@ -159,10 +248,6 @@ fun Board(state: Pair<Lander, Pair<List<Pair<Double, Double>>, Double>>, onFrame
             x = state.first.position.first.toFloat(),
             y = state.first.position.second.toFloat())
 
-//        drawText(
-//            topLeft = Offset(10f, 10f),
-//            textLayoutResult = textLayoutFps
-//        )
 
         drawText(
             topLeft = Offset(size.width - 100f, 10f),
@@ -176,12 +261,18 @@ fun Board(state: Pair<Lander, Pair<List<Pair<Double, Double>>, Double>>, onFrame
 
         scale(20f, -20f) {
             translate(left = size.width/2 - state.first.position.first.toFloat(), top = size.height/2 - state.first.position.second.toFloat()) {
-                state.second.first.zipWithNext { a, b ->
+                val extendedPoints = mutableListOf<Pair<Double, Double>>()
+                val points = state.second.first
+
+                extendedPoints.addAll(points.takeLast(80).map {pair -> Pair(pair.first - points.last().first, pair.second) })
+                extendedPoints.addAll(points)
+                extendedPoints.addAll(points.take(80).map {pair -> Pair(pair.first + points.last().first, pair.second) })
+
+                extendedPoints.zipWithNext { a, b ->
                     drawLine(
                         color = Color.Gray,
-                        start = Offset(a.first.toFloat(), a.second.toFloat()),
-                        end = Offset(b.first.toFloat(), b.second.toFloat()),
-                        strokeWidth = 0.5f,
+                        start = Offset(a.first.toFloat(), a.second.toFloat() - 0.3f),
+                        end = Offset(b.first.toFloat(), b.second.toFloat() - 0.3f),
                         cap = StrokeCap.Round)
                 }
 
@@ -258,48 +349,8 @@ fun Board(state: Pair<Lander, Pair<List<Pair<Double, Double>>, Double>>, onFrame
 
 
 fun main() = application {
-    val game = remember { Game() }
     Window(onCloseRequest = ::exitApplication,
-        onKeyEvent = {
-            when (it.key) {
-                Key.A -> {
-                    when (it.type) {
-                        KeyEventType.KeyDown -> {
-                            game.rotationToApply = -25.0
-                        }
-                        KeyEventType.KeyUp -> {
-                            game.rotationToApply = 0.0
-                        }
-                    }
-                    true
-                }
-                Key.D -> {
-                    when (it.type) {
-                        KeyEventType.KeyDown -> {
-                            game.rotationToApply = 25.0
-                        }
-                        KeyEventType.KeyUp -> {
-                            game.rotationToApply = 0.0
-                        }
-                    }
-                    true
-                }
-                Key.W -> {
-                    when (it.type) {
-                        KeyEventType.KeyDown -> {
-                            game.engineTime = 1.0
-                        }
-                        KeyEventType.KeyUp -> {
-                            game.engineTime = 0.0
-                        }
-                    }
-                    true
-                }
-                else -> {
-                    false
-                }
-            }
-        }) {
-        App(game)
+    ) {
+        App()
     }
 }
